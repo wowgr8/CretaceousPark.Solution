4f3046f531a7fd56db79433704a994c5ed7bf13b
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-04-10 17:07:23 -0700

    add forgotten return for Put functionality. Also rearranged using directived (removed a duplicated using directive)

--- CretaceousPark/Controllers/AnimalsControllers.cs
+++ CretaceousPark/Controllers/AnimalsControllers.cs
@@ -1,11 +1,12 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using CretaceousPark.Models;
 // allows use of EntityState
 using Microsoft.EntityFrameworkCore;
+using CretaceousPark.Models;
 
 namespace CretaceousPark.Controllers
 {
@@ -74,6 +75,8 @@ namespace CretaceousPark.Controllers
           throw;
         }
       }
+
+      return NoContent();
     }
 
     // POST api/animals
